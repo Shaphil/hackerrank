a9ba80ab5f49bf73abbc36bdb6aceec50184a5ed
Shaphil Mahmud <shaphil2025@gmail.com>
2017-01-29 01:02:43 +0600

    Data Structures - 2D Array - Solved

--- Data Structures/Arrays/2D array - DS.c
+++ Data Structures/Arrays/2D array - DS.c
@@ -2,13 +2,14 @@
 
 int main()
 {
-	freopen("2d_array.txt", "r", stdin);
+	// freopen("2d_array.txt", "r", stdin);
 	
 	int a[6][6];
 	int i, j;
 	int sum, highest;
 	
-	sum = highest = 0;
+	sum = 0;
+	highest = -2147483648;
 	
 	for(i = 0; i < 6; i++)
 		for(j = 0; j < 6; j++)
@@ -18,27 +19,18 @@ int main()
 		
 	for(i = 0; i < 4; i++) {
 		for(j = 0; j < 4; j++) {
-			printf("%d %d %d\n", a[i][j], a[i][j + 1], a[i][j + 2]);
-			
 			sum += ( a[i][j] + a[i][j + 1] + a[i][j + 2] ) ;
-			
-			printf("x %d x\n", a[i + 1][j + 1]);
 			sum += a[i + 1][j + 1];
-			
-			printf("%d %d %d\n", a[i + 2][j], a[i + 2][j + 1], a[i + 2][j + 2]);
 			sum += ( a[i + 2][j] + a[i + 2][j + 1] + a[i + 2][j + 2] );
-			
-			printf("sum: %d\n", sum);
 			
 			if(sum > highest)
 				highest = sum;
 		
 			sum = 0;
 		}
-		printf("\n");
 	}
 	
-	printf("Highest: %d\n", highest);
+	printf("%d\n", highest);
 	
 	return 0;
 }
