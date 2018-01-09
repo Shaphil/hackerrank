1f6163ec7a143bda00514eb0a968dfde1b141cd9
Shaphil <shaphil2025@gmail.com>
2018-01-09 23:45:15 +0600

    (Update~2) Algorithms: Implementation - 01. Grading Students

--- Algorithms/02_Implementation/01_grading_students.c
+++ Algorithms/02_Implementation/01_grading_students.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void solve(int grades_size, int* grades){
-    for (int i = 0; i < grades_size; i++) {
-        if (grades[i] >= 38) {
-            int div = grades[i] / 5;
-            int multiple = (div + 1) * 5;
-            if (multiple - grades[i] < 3)
-                grades[i] = multiple;
-        }
-    }
-}
+int main()
+{
+    int n, grade, div, multiple;
 
-int main() {
-    int n;
     scanf("%d", &n);
-    int *grades = malloc(sizeof(int) * n);
-    for(int grades_i = 0; grades_i < n; grades_i++){
-       scanf("%d",&grades[grades_i]);
-    }
-    solve(n, grades);
-    for(int i = 0; i < n; i++) {
-        if(i) {
-            printf("\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &grade);
+        if (grade >= 38) {
+            div = grade / 5;
+            multiple = (div + 1) * 5;
+            if (multiple - grade < 3)
+                grade = multiple;
         }
-        printf("%d", grades[i]);
+        printf("%d\n", grade);
     }
-    puts("");
-
 
     return 0;
 }
